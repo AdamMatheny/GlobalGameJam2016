d572ef228c517fa4a3bfaf4ff7dab286391264dc
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2016-01-30 15:17:22 -0500

    CrossHair Change
    Made the crosshair unable to touch the player.

--- GGJ2016 Source/Assets/__Scripts/Arena/Environment/Crosshair.cs
+++ GGJ2016 Source/Assets/__Scripts/Arena/Environment/Crosshair.cs
@@ -20,6 +20,8 @@ public class Crosshair : MonoBehaviour {
         x = Input.GetAxis("Mouse X");
         y = Input.GetAxis("Mouse Y");
 
-        transform.localPosition = new Vector3((x * 4), (y * 4), transform.localPosition.z);
+        if(x > .2f || y > .2f || x < -.2f || y < -.2f)
+
+        transform.localPosition = new Vector3((x * 5), (y * 4), transform.localPosition.z);
     }
 }
