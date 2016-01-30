4af65e914e1e49a8271dbece1ddebb946bec4074
Nidaoke <jnidaokefantagefacebook27@gmail.com>
2016-01-30 12:29:59 -0500

    Random enemy spawning

---
+++ GGJ2016 Source/Assets/__Scripts/Arena/Environment/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public float percent;
+
+    public ArenaBullSpawner bullSpawner;
+
+    public GameObject enemy;
+
+    public float enemyAmount;
+
+    public ArenaPlayer mTargetPlayer;
+
+    // Use this for initialization
+    void Start () {
+
+        if (mTargetPlayer == null)
+        {
+            mTargetPlayer = FindObjectOfType<ArenaPlayer>();
+        }
+
+        if (bullSpawner == null)
+        {
+            bullSpawner = FindObjectOfType<ArenaBullSpawner>();
+        }
+
+        enemyAmount = Mathf.Round(mTargetPlayer.GetComponent<ArenaPlayer>().tomatoCount * (percent / 100));
+
+        for (int i = 0; i < enemyAmount; i++)
+        {
+            GameObject spawnGate = null;
+            if (bullSpawner.mBullExits.Count > 0)
+            {
+                spawnGate = bullSpawner.mBullExits[Mathf.FloorToInt(Random.Range(0, bullSpawner.mBullExits.Count))].gameObject;
+            }
+
+            if (spawnGate != null)
+            {
+                Instantiate(enemy, spawnGate.transform.position, Quaternion.identity);
+            }
+        }
+
+        // foreach()
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
