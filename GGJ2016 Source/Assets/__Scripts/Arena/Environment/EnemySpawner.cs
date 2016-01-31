f28a2634951a9ae4012fe51a53d4954caeac6801
AdamMatheny <adamamatheny@gmail.com>
2016-01-31 03:36:10 -0500

    -Minor adjustment to EnemySpawner scalign

--- GGJ2016 Source/Assets/__Scripts/Arena/Environment/EnemySpawner.cs
+++ GGJ2016 Source/Assets/__Scripts/Arena/Environment/EnemySpawner.cs
@@ -88,7 +88,7 @@ public class EnemySpawner : MonoBehaviour
 				mArenaSpawnTimer = mArenaSpawnTimerDefault;
 				if(mArenaSpawnTimerDefault >0.1f)
 				{
-					mArenaSpawnTimerDefault -=0.2f;
+					mArenaSpawnTimerDefault -= (0.1 +(PlayerPrefs.GetInt("ArenaRound")*0.05f) );
 				}
 			}
 		}
