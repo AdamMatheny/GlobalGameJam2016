1c3147c13bcc89aae0ef4f788d30d7b82513dd3c
HalfABomb <s.craig82@yahoo.com>
2016-01-31 00:19:11 -0500

    Added Baby Jumping code
    The player should now be able to jump over the baby and experience some
    kind of feedback(the level restarts)

---
+++ temporary GitHub dump/Assets/ChangeSkyColor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSkyColor : MonoBehaviour {
+
+    public float time;
+    public float timer;
+
+    public bool goBlack = true;
+
+	// Use this for initialization
+	void Start () {
+
+        timer = time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timer > 0)
+        {
+
+            timer -= Time.timeScale;
+        }
+        else
+        {
+
+            goBlack = !goBlack;
+            timer = time;
+        }
+
+        
+
+        if (goBlack)
+        {
+
+            gameObject.GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, Color.black, 0.003f);
+        }
+        else
+        { 
+            gameObject.GetComponent<SpriteRenderer>().color = Color.Lerp(GetComponent<SpriteRenderer>().color, Color.white, 0.003f);
+
+        }
+
+    }
+}
---
+++ temporary GitHub dump/Assets/__Scripts/General/HighscoreEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class HighscoreEntry : IComparable
+{
+	private string mName;
+	private int mScore;
+	public HighscoreEntry (String name, int score)
+	{
+		mName = name;
+		mScore = score;
+	}
+
+	public string GetName() { return mName; } 
+	public int GetScore() {
+		return mScore;
+	}
+
+	public Dictionary<String, System.Object> asDictionary() {
+		Dictionary<String, System.Object> entry = new Dictionary<String, System.Object> ();
+		entry["name"] = GetName();
+		entry["score"] = GetScore();
+		return entry;
+	}
+
+	//Used by sort
+	int IComparable.CompareTo(object obj)
+	{
+		HighscoreEntry entry=(HighscoreEntry)obj;
+		return entry.GetScore () - this.GetScore ();
+	}
+}
+
